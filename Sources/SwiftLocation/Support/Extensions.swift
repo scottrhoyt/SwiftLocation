a714c5a1396c2102ad180f9b944ecacff6124796
Scott Hoyt <scottrhoyt@gmail.com>
2025-06-07 11:37:48 -0700

    Add retroactive to protocol-conformance-adding extensions.

--- Sources/SwiftLocation/Support/Extensions.swift
+++ Sources/SwiftLocation/Support/Extensions.swift
@@ -60,7 +60,7 @@ extension CLLocationManager: LocationManagerProtocol {
     
 }
 
-extension CLAccuracyAuthorization: CustomStringConvertible {
+extension CLAccuracyAuthorization: @retroactive CustomStringConvertible {
     
     public var description: String {
         switch self {
@@ -75,7 +75,7 @@ extension CLAccuracyAuthorization: CustomStringConvertible {
     
 }
 
-extension CLAuthorizationStatus: CustomStringConvertible {
+extension CLAuthorizationStatus: @retroactive CustomStringConvertible {
     
     public var description: String {
         switch self {
